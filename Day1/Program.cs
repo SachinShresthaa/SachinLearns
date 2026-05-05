ccf9d9346fa04d7dd3e10f0d9e9da5a2fe95a859
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-05 21:47:31 +0545

    First Commit

---
+++ Day1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a, b, c;
+        double sum, mul, div;
+
+        // Input
+        Console.Write("Enter first number: ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        c = Convert.ToDouble(Console.ReadLine());
+
+        // Calculations
+        sum = a + b + c;
+        mul = a * b * c;
+
+        // Check for division by zero
+        if (b != 0 && c != 0)
+        {
+            div = (a / b) / c;
+        }
+        else
+        {
+            Console.WriteLine("Division not possible (division by zero).");
+            div = 0;
+        }
+
+        // Output
+        Console.WriteLine("\nSum = " + sum);
+        Console.WriteLine("Multiplication = " + mul);
+        Console.WriteLine("Division = " + div);
+
+        Console.ReadLine();
+    }
+}
