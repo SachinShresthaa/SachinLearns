2590c1a3bd892562d8013977bc79fa18501904d5
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-08 22:26:44 +0545

    all updates

--- C#/Day1/TypeConversion.cs
+++ C#/Day1/TypeConversion.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Net;
-class TypeConversion
-{
-    static void Main (string[] args)
-    {
-        Console.Write("enter number");
-        string number = Console.ReadLine();
-        int n = Convert.ToInt32(number);
-        Console.WriteLine("The conversion vaule is:"+n);
-    }
-}
+﻿// using System;
+// using System.Net;
+// class TypeConversion
+// {
+//     static void Main (string[] args)
+//     {
+//         Console.Write("enter number");
+//         string number = Console.ReadLine();
+//         int n = Convert.ToInt32(number);
+//         Console.WriteLine("The conversion vaule is:"+n);
+//     }
+// }
--- C#/Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
+++ C#/Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2bc8b2745b436a7423ba3c2258c86931d5048fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8fe60532c637f3510be415853e96b3832477ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
