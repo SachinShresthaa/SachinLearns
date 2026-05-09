eec754e14ff00cdc1363a7381b6e63e9c416d29e
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-09 22:08:09 +0545

    remaining all for basic

--- C#/Basic/MainMethod.cs
+++ C#/Basic/MainMethod.cs
@@ -1,17 +1,26 @@
 using System;
+using System.Globalization;
 using System.Reflection.Metadata.Ecma335;
 class MainMethod
 {
     static void Main (String[] args)
     {
-        Function.Greet();
+        // Function.Greet();
         
-        Function.DisplayName("Sachin");
+        // Function.DisplayName("Sachin");
 
-        int number = Function.GetNumber();
-        Console.WriteLine("The Value is:"+ number);
+        // int number = Function.GetNumber();
+        // Console.WriteLine("The Value is:"+ number);
 
-        int SUM = Function.sum(2,3); 
-        Console.WriteLine("The sume is :"+SUM);
-    }
+        // int SUM = Function.sum(2,3); 
+        // Console.WriteLine("The sume is :"+SUM);
+
+        //StringsMethods.AllStringMethodHere();
+
+        // CollectionExample.ShowCollectionExample();
+
+        // Calculator.StartCalculator();    
+        
+        ArrayExample.ShowArrayOperations();
+        }
 }
--- C#/Basic/obj/Debug/net10.0/Day1.AssemblyInfo.cs
+++ C#/Basic/obj/Debug/net10.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8fe60532c637f3510be415853e96b3832477ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d38d39776492c90761dcaf1a6b9a0633593fdf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
