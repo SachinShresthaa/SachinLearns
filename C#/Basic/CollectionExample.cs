13d01dfca57fe11d2afcd4a291df7b36d8da73af
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-09 09:58:46 +0545

    Collection practised

---
+++ C#/Basic/CollectionExample.cs
@@ -0,0 +1,25 @@
+class CollectionExample
+{
+    public static void ShowCollectionExample(){
+        List<String> names = new List<String>();
+        names.Add("Sachin");
+        names.Add("Amgel");
+        names.Add("Prasun");
+        
+        foreach(string name in names)
+        {
+          Console.WriteLine(name);  
+        }
+
+        Dictionary<int,String> Fruits = new Dictionary<int, string>();
+        Fruits.Add(1,"Apple");
+        Fruits.Add(2,"Banana");
+        Fruits.Add(3,"Mango");
+
+        foreach(var fruit in Fruits)
+        {
+            Console.WriteLine(fruit.Key+":"+fruit.Value);
+        }
+
+            }
+}
