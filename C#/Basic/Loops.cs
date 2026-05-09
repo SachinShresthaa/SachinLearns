182d98eba3c4822069bf205d5b6bfc841296f7f6
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-09 10:00:17 +0545

    Comment Loops

--- C#/Basic/Loops.cs
+++ C#/Basic/Loops.cs
@@ -1,57 +1,57 @@
-using System;
+// using System;
 
-class AllLoops
-{
-    static void Main(string[] args)
-    {
-        // FOR LOOP
-        Console.WriteLine("FOR LOOP");
+// class AllLoops
+// {
+//     static void Main(string[] args)
+//     {
+//         // FOR LOOP
+//         Console.WriteLine("FOR LOOP");
 
-        for(int i = 1; i <= 5; i++)
-        {
-            Console.WriteLine(i);
-        }
+//         for(int i = 1; i <= 5; i++)
+//         {
+//             Console.WriteLine(i);
+//         }
 
-        Console.WriteLine();
-
-
-        // WHILE LOOP
-        Console.WriteLine("WHILE LOOP");
-
-        int j = 1;
-
-        while(j <= 5)
-        {
-            Console.WriteLine(j);
-            j++;
-        }
-
-        Console.WriteLine();
+//         Console.WriteLine();
 
 
-        // DO-WHILE LOOP
-        Console.WriteLine("DO-WHILE LOOP");
+//         // WHILE LOOP
+//         Console.WriteLine("WHILE LOOP");
 
-        int k = 1;
+//         int j = 1;
 
-        do
-        {
-            Console.WriteLine(k);
-            k++;
-        }
-        while(k <= 5);
+//         while(j <= 5)
+//         {
+//             Console.WriteLine(j);
+//             j++;
+//         }
 
-        Console.WriteLine();
+//         Console.WriteLine();
 
 
-        // FOREACH LOOP
-        Console.WriteLine("FOREACH LOOP");
+//         // DO-WHILE LOOP
+//         Console.WriteLine("DO-WHILE LOOP");
 
-        string[] fruits = {"Apple", "Banana", "Mango"};
+//         int k = 1;
 
-        foreach(string fruit in fruits)
-        {
-            Console.WriteLine(fruit);
-        }
-    }
-}
+//         do
+//         {
+//             Console.WriteLine(k);
+//             k++;
+//         }
+//         while(k <= 5);
+
+//         Console.WriteLine();
+
+
+//         // FOREACH LOOP
+//         Console.WriteLine("FOREACH LOOP");
+
+//         string[] fruits = {"Apple", "Banana", "Mango"};
+
+//         foreach(string fruit in fruits)
+//         {
+//             Console.WriteLine(fruit);
+//         }
+//     }
+// }
