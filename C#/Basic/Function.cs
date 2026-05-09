50b81629d59baf8dded62ad1c19ce930126e196e
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-09 08:44:05 +0545

    Function Learned all types

--- C#/Basic/Function.cs
+++ C#/Basic/Function.cs
@@ -4,4 +4,17 @@ class Function
     {
         Console.WriteLine("Hello Angel, nice To meet Youuuu");
     }
+    public static void DisplayName(String name)
+    {
+        Console.WriteLine("Displpayed name is :"+ name);
+    }
+    public static int GetNumber()
+    {
+        return 100;
+    }
+    public static int sum(int a, int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
 }
---
+++ C#/Basic/MainMethod.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+class MainMethod
+{
+    static void Main (String[] args)
+    {
+        Function.Greet();
+        
+        Function.DisplayName("Sachin");
+
+        int number = Function.GetNumber();
+        Console.WriteLine("The Value is:"+ number);
+
+        int SUM = Function.sum(2,3); 
+        Console.WriteLine("The sume is :"+SUM);
+    }
+}
