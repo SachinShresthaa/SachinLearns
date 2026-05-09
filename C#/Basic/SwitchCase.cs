4d5e695a0a1f580f9a2d4717495819d44ce8a02b
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-09 07:35:01 +0545

    Folder changed

---
+++ C#/Basic/SwitchCase.cs
@@ -0,0 +1,28 @@
+// using System;
+// class SwitchCase
+// {
+//     static void Main (string[] args)
+//     {
+//         Console.Write("Enter the day");
+//         int day = Convert.ToInt32(Console.ReadLine());
+
+// switch(day)
+// {
+//     case 1:
+//         Console.WriteLine("Sunday");
+//         break;
+
+//     case 2:
+//         Console.WriteLine("Monday");
+//         break;
+
+//     case 3:
+//         Console.WriteLine("Tuesday");
+//         break;
+
+//     default:
+//         Console.WriteLine("Invalid");
+//         break;
+// }
+//     }
+// }
---
+++ C#/Basic/TypeConversion.cs
@@ -0,0 +1,12 @@
+﻿// using System;
+// using System.Net;
+// class TypeConversion
+// {
+//     static void Main (string[] args)
+//     {
+//         Console.Write("enter number");
+//         string number = Console.ReadLine();
+//         int n = Convert.ToInt32(number);
+//         Console.WriteLine("The conversion vaule is:"+n);
+//     }
+// }
