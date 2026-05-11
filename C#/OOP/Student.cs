964275045831237724a645ef39bcb377c28c2d05
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-11 19:13:12 +0545

    Advance challenges Complete hahahahha

--- C#/OOP/Student.cs
+++ C#/OOP/Student.cs
@@ -1,7 +1,29 @@
 class Student
 {
-    public Student()
+    public string name;
+    public int rollno;
+    public string faculty;
+    public double marks;
+    public Student(string name, int rollno, string faculty, double marks)
     {
-        Console.WriteLine("Constructor called");
+        this.name=name;
+        this.rollno=rollno;
+        this.faculty=faculty;
+        this.marks=marks;
     }
+
+    public Student(string name, int rollno)
+    {
+    this.name=name;
+    this.rollno=rollno;
+    faculty="unknown";
+    marks=0;    
+    }
+    public void DisplayStudentInfo()
+    {
+        Console.WriteLine("Name: "+name);
+        Console.WriteLine("Rollno: "+rollno);
+        Console.WriteLine("Faculty: "+faculty);
+        Console.WriteLine("Marks: "+marks);    
+        }
 }
