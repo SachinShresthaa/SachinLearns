92684506cbacef42fa6494e876b6973697f95770
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-11 19:51:21 +0545

    main method code

---
+++ C#/OOP/MainMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+class MainMethod
+{
+    static void Main(string[] args)
+    {
+        // ClassAndObject obj = new ClassAndObject();
+        // obj.Display("Sachin");
+
+        // Student s1 = new Student("sachin",22,"BCA",99.99);
+        // Student s2 = new Student("Angel", 20);
+        // s1.DisplayStudentInfo();
+        // s2.DisplayStudentInfo();
+    }
+}
