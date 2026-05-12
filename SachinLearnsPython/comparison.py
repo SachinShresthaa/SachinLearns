ebc7f3075c86bbe400688871493f5fea6bd3fe2f
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-12 09:22:55 +0545

    Comparision if else learned

---
+++ SachinLearnsPython/comparison.py
@@ -0,0 +1,15 @@
+age=17
+if age<18:
+    print(f"Minor")
+else:
+    print("18+")
+
+saved_password="sachinhero"
+entered = "sachinzero"
+
+if entered==saved_password:
+    print("Correct password")
+else:
+    print("incorrect Password")
+
+    
