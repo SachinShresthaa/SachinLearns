e9506eeff131d6b1e6daf84d1e35a39b3d0021d0
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-12 17:54:42 +0545

    Challange Inheritance complete

--- C#/OOP/Inheritance/MainMethod.cs
+++ C#/OOP/Inheritance/MainMethod.cs
@@ -7,6 +7,10 @@ class MainMethod
         d1.DisplayDoctorInfo();
         d1.DisplayPersonInfo();
 
+        Doctor d2 = new Doctor("Sinzo",22, "Liver" ,50000);
+        d2.DisplayDoctorInfo();
+        d2.DisplayPersonInfo();
+
         Patient p1 = new Patient("Angel",20,"Headache",7);
         p1.DisplayPatientInfo();
         p1.DisplayPersonInfo();
