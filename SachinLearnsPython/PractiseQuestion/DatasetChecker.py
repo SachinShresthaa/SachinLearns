2eaf84d6109393572123b98e7be14c392a82bd9d
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-16 13:58:06 +0545

    Complete practise question 3

---
+++ SachinLearnsPython/PractiseQuestion/DatasetChecker.py
@@ -0,0 +1,17 @@
+tr = int (input("Enter the total rows: "))
+mr = int (input("Enter the Missing rows: "))
+dr = int (input ("Enter the Duplicate rows: "))
+
+cleanData = tr-(mr+dr)
+cleanDataPercentage = float(cleanData/tr)*100
+
+print(f"\nClean Data Percentage: {cleanDataPercentage:.2f}%")
+
+if cleanData >= 95:
+    res = "Production ready"
+elif cleanData<95 and cleanData>=80:
+    res = "Needs Cleaning"
+else:
+    res = "Poor Data set"
+
+print(f"\nDataset Status:: {res}")
