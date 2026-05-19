f9b73a2bc014c002eb37ae070d181ed886d97a28
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-19 19:04:46 +0545

    Displayed all data in loops and stored in array

--- C#/AbstractionAndInterface/MainMethod.cs
+++ C#/AbstractionAndInterface/MainMethod.cs
@@ -1,26 +1,35 @@
-using System;
-using System.Collections.Generic;
-
 class MainMethod
 {
     static void Main(string[] args)
     {
-        List<ISwitchable> devices =
-            new List<ISwitchable>();
+        // List<ISwitchable> devices =
+        //     new List<ISwitchable>();
 
-        devices.Add(
-            new SmartLight("Bedroom Light")
-        );
+        // devices.Add(
+        //     new SmartLight("Bedroom Light")
+        // );
 
-        devices.Add(
-            new SmartFan("Hall Fan")
-        );
+        // devices.Add(
+        //     new SmartFan("Hall Fan")
+        // );
 
-        foreach(ISwitchable device in devices)
+        // foreach(ISwitchable device in devices)
+        // {
+        //     device.TurnOn();
+
+        //     Console.WriteLine();
+        // }
+
+        List<Vehicle> vehicles = new List<Vehicle>();
+        vehicles.Add( new CarRide("Manish",009));
+        vehicles.Add( new BikeRide("Bhupen",010));
+        vehicles.Add( new PremiumRide("Sumit",011));
+
+        foreach(Vehicle vel in vehicles)
         {
-            device.TurnOn();
-
-            Console.WriteLine();
+            vel.ShowVehicleInfo();
+            Console.WriteLine("Fare: "+vel.CalculateFare(50));
         }
+
     }
 }
