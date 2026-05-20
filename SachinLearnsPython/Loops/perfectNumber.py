55fb543f36e2d64aa7dc9b7333c29b67b1214bc6
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-20 20:22:41 +0545

    Error Solve

---
+++ SachinLearnsPython/Loops/perfectNumber.py
@@ -0,0 +1,8 @@
+#Print all perfect Numbers between 1 and 500. A perfect number equals the sum of its proper divisions
+for n in range(1,500):
+    div_sum = 0
+    for d in range(1,n):
+        if n % d == 0:
+            div_sum += d
+    if div_sum==n:
+        print(n,end=" ")
