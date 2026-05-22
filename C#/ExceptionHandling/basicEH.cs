0dd88c0c662776eacae3c947e07406ae71596cc6
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-22 20:31:48 +0545

    baisic Exception handling

---
+++ C#/ExceptionHandling/basicEH.cs
@@ -0,0 +1,24 @@
+using System;
+
+class basicEH
+{
+   public void EH()
+    {
+        try
+        {
+            int a = 10;
+            int b = 0;
+
+            int result = a / b;
+
+            Console.WriteLine(result);
+        }
+
+        catch(Exception ex)
+        {
+            Console.WriteLine(
+                "Error: " + ex.Message
+            );
+        }
+    }
+}
---
+++ C#/ExceptionHandling/mainMethod.cs
@@ -0,0 +1,10 @@
+using System.Security.Cryptography.X509Certificates;
+
+class MainMethod
+{
+    public static void Main(String[] args)
+    {
+        basicEH b = new basicEH();
+        b.EH();
+    }
+}
