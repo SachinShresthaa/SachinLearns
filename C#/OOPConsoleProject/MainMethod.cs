47807e57595b4ddf556f70aea16180cc51b10563
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-24 15:47:28 +0545

    Doctor detail user input

---
+++ C#/OOPConsoleProject/MainMethod.cs
@@ -0,0 +1,53 @@
+public class mainMethod
+{
+    static void Main(string[] args)
+    {
+        List<Person> p = new List<Person>();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\n===== Hospital Management System =====");
+            Console.WriteLine("1. Add Doctor");
+            Console.WriteLine("2. Add Patient");
+            Console.WriteLine("3. Show All Records");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter Choice: ");
+            int Choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (Choice)
+            {
+                case 1:
+                    Console.Write("Enter Doctor Name: ");
+                    string dname =Console.ReadLine();
+
+                    Console.Write( "Enter Doctor Age: ");
+                    int dage =Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter Gender: ");
+                    string dgender =Console.ReadLine();
+
+                    Console.Write("Enter Doctor ID: ");
+                    int did =Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter Specialization: ");
+                    string specialization =Console.ReadLine();
+
+                    Console.Write("Enter Salary: ");
+                    double salary =Convert.ToDouble(Console.ReadLine());
+                    
+                    Doctor d1 =new Doctor(dname,dage,dgender,did,specialization,salary);
+                    people.Add(d1);
+                    Console.WriteLine("Doctor Added Successfully");
+
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+            }
+        }
+    }
+}
