31e2541918657142c78f8fa7765a10f83d8aa3a7
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-26 09:39:13 +0545

    print vs return code

---
+++ SachinLearnsPython/Function/printVsreturn.py
@@ -0,0 +1,18 @@
+#using print()
+
+def add(a,b):
+    print(a+b)
+
+result = add(3,4)
+print(result)
+
+final = result + 10
+print(final)
+
+#using return
+def add(a,b):
+    return a+b
+result = add(3,4)
+print(result)
+final = result + 10
+print (final)
