c81357b625052f1a99409d3e52ab95c70045157e
Sachin Shrestha <sachins.newa1@gmail.com>
2026-06-01 23:30:53 +0545

    ai label counter

---
+++ SachinLearnsPython/assignment2/labelCounter.py
@@ -0,0 +1,14 @@
+def count_positive_labels(labels):
+    count = 0
+    for label in labels:
+        if label == 1:
+            count += 1
+    return count
+
+labels = [1, 0, 1, 1, 0, 1, 0, 0, 1, 1]
+positive_count = count_positive_labels(labels)
+
+percentage = (positive_count / len(labels)) * 100
+
+print("Positive Labels:", positive_count)
+print("Percentage of Positive Labels:", percentage, "%")
