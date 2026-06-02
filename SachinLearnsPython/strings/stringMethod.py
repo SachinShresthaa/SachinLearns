fce26020c44126d6d61101636d64e49c29b8c569
Sachin Shrestha <sachins.newa1@gmail.com>
2026-06-02 09:29:28 +0545

    string method

---
+++ SachinLearnsPython/strings/stringMethod.py
@@ -0,0 +1,9 @@
+name = " sachin shrestha "
+print(name.upper())
+print(name.lower())
+print(name.strip())
+print(name.split(" "))
+print(name.count("s"))
+print(name.replace("s","D"))
+print(name.find("n"))
+print(" ".join(["angel", "Lamichhane"]))
