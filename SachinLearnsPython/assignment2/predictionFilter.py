34fa37b391bc0bccdd3bd2e96180b5c01af77c3a
Sachin Shrestha <sachins.newa1@gmail.com>
2026-06-05 12:19:16 +0545

    filter high confidence scores

--- SachinLearnsPython/assignment2/predictionFilter.py
+++ SachinLearnsPython/assignment2/predictionFilter.py
@@ -1,5 +1,5 @@
 scores = [0.45, 0.92, 0.88, 0.65, 0.99, 0.71]
 
-filtered_scores = list(filter(lambda score: score > 0.80, scores))
+filteredScores = list(filter(lambda score: score > 0.80, scores))
 
-print("Filtered confidence scores (> 0.80):", filtered_scores)
+print("Filtered confidence scores:", filteredScores)
