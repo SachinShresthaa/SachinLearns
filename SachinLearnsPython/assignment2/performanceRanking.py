0eb9f3b18f89c038595cb37a8f3b925e19e8d73d
Sachin Shrestha <sachins.newa1@gmail.com>
2026-06-05 12:19:14 +0545

    student grade performance ranker

--- SachinLearnsPython/assignment2/performanceRanking.py
+++ SachinLearnsPython/assignment2/performanceRanking.py
@@ -1,3 +1,14 @@
+# Create a function that accepts a list of marks. 
+# ● Use loops to calculate grades: 
+# ○ A → 80 and above 
+# ○ B → 60–79 
+# ○ C → 40–59 
+# ○ F → Below 40 
+# ● Use a lambda function to sort marks in descending order. 
+# ● Display: 
+# ○ Sorted marks 
+# ○ Grade distribution 
+# ○ Number of passed students
 def process_marks(marks):
     sorted_marks = sorted(marks, key=lambda x: x, reverse=True)
 
