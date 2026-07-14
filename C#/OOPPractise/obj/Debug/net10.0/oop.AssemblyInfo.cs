967868b78a9d888d4c82ff51bd0aff5f8d6097e8
Sachin Shrestha <sachins.newa1@gmail.com>
2026-07-14 22:46:37 +0545

    all add

--- C#/OOPPractise/obj/Debug/net10.0/oop.AssemblyInfo.cs
+++ C#/OOPPractise/obj/Debug/net10.0/oop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ae33aa2a8a376f4852157f15071aafd07a3b71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2627eaba0933bb91bda3cbd7854bf39a2783e08")]
 [assembly: System.Reflection.AssemblyProductAttribute("oop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
