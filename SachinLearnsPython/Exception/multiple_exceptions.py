719ea6bf5265c787f3009bc1f9d99e85fa8070d5
Sachin Shrestha <sachins.newa1@gmail.com>
2026-07-15 20:41:35 +0545

    multiple exceptoin

---
+++ SachinLearnsPython/Exception/multiple_exceptions.py
@@ -0,0 +1,13 @@
+# multiple_exceptions.py
+
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    print(num1 / num2)
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+except ValueError:
+    print("Please enter valid integers.")
