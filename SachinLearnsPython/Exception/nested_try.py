d0b6f7b47dea44d45f37c426f00e7d315a8381f1
Sachin Shrestha <sachins.newa1@gmail.com>
2026-07-22 20:38:37 +0545

    nested try

---
+++ SachinLearnsPython/Exception/nested_try.py
@@ -0,0 +1,12 @@
+# nested_try.py
+
+try:
+    try:
+        num = int(input("Enter number: "))
+        print(100 / num)
+
+    except ZeroDivisionError:
+        print("Cannot divide by zero.")
+
+except ValueError:
+    print("Invalid input.")
