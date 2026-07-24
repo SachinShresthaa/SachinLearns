5b5bb63ceb9b8c8eeb3e016badc16a4338f685bd
Sachin Shrestha <sachins.newa1@gmail.com>
2026-07-24 10:00:56 +0545

    switch function

--- SachinLearnsPython/student_record_system.py
+++ SachinLearnsPython/student_record_system.py
@@ -72,3 +72,39 @@ def delete_student():
 
     except ValueError:
         print("\nPlease enter a valid Student ID.")
+
+
+while True:
+
+    print("""
+==============================
+    STUDENT RECORD SYSTEM
+==============================
+
+1. Add Student
+2. View Students
+3. Search Student
+4. Delete Student
+5. Exit
+""")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        add_student()
+
+    elif choice == "2":
+        view_students()
+
+    elif choice == "3":
+        search_student()
+
+    elif choice == "4":
+        delete_student()
+
+    elif choice == "5":
+        print("Program closed.")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
