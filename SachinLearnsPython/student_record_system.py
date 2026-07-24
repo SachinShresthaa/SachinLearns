e023f1b30fc8b9eaaf2e404e5cade4ec5a1db7df
Sachin Shrestha <sachins.newa1@gmail.com>
2026-07-24 09:59:20 +0545

    search stdent

--- SachinLearnsPython/student_record_system.py
+++ SachinLearnsPython/student_record_system.py
@@ -22,18 +22,37 @@ def add_student():
     except ValueError:
         print("\nPlease enter valid numbers for ID and Age.")
 
-    def view_students():
-        if len(students) == 0:
-            print("\nNo student records found.")
+
+def view_students():
+    if len(students) == 0:
+        print("\nNo student records found.")
         return
 
     print("\n----- Student Records -----")
 
     for student in students:
         print(f"""
-            ID: {student["id"]}
-            Name: {student["name"]}
-            Age: {student["age"]}
-            Course: {student["course"]}
-            ---------------------------
-            """)
+ID: {student["id"]}
+Name: {student["name"]}
+Age: {student["age"]}
+Course: {student["course"]}
+---------------------------
+""")
+
+
+def search_student():
+    try:
+        student_id = int(input("Enter Student ID to search: "))
+
+        for student in students:
+            if student["id"] == student_id:
+                print("\nStudent Found!")
+                print("Name:", student["name"])
+                print("Age:", student["age"])
+                print("Course:", student["course"])
+                return
+
+        print("\nStudent not found.")
+
+    except ValueError:
+        print("\nPlease enter a valid Student ID.")
