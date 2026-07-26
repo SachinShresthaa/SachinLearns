d0ee4e968a0066b364e4f2a113c8867da89872db
Sachin Shrestha <sachins.newa1@gmail.com>
2026-07-26 22:56:09 +0545

    parent class with function display info: :

--- SachinLearnsPython/bank_management_system.py
+++ SachinLearnsPython/bank_management_system.py
@@ -1,1 +1,10 @@
 import json
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
