1fba55a3ab954b99bd0ea43de8c8cfdf99f4f1eb
Sachin Shrestha <sachins.newa1@gmail.com>
2026-07-30 23:47:08 +0545

    function to deposit money

--- SachinLearnsPython/bank_management_system.py
+++ SachinLearnsPython/bank_management_system.py
@@ -135,3 +135,29 @@ Balance: ${self.__balance:.2f}
                 return account
 
         return None
+
+    def deposit_money(self):
+
+        try:
+
+            account_number = int(
+                input("Enter Account Number: ")
+            )
+
+            account = self.find_account(account_number)
+
+            if account is None:
+
+                print("Account not found.")
+
+                return
+
+            amount = float(input("Enter amount to deposit: "))
+
+            account.deposit(amount)
+
+            self.save_accounts()
+
+        except ValueError as error:
+
+            print("Error:", error)
