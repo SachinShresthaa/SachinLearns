32c1590538ee3f7eb209876e52aadf2c2385741f
Sachin Shrestha <sachins.newa1@gmail.com>
2026-08-03 19:52:00 +0545

    order:

--- SachinLearnsPython/restaurant_management_system.py
+++ SachinLearnsPython/restaurant_management_system.py
@@ -16,3 +16,16 @@ class FoodItem:
         return self.__price
     def display(self):
         print(f"{self.item_id}. {self.name:<15} Rs.{self.__price}")
+
+class Order:
+    total_sales=0
+    def __init__(self,customer,food,qty):
+        self.customer=customer
+        self.food=food
+        self.qty=qty
+        self.total=food.get_price()*qty
+        Order.total_sales+=self.total
+    def to_dict(self):
+        return {"customer":self.customer.name,"food":self.food.name,"qty":self.qty,"total":self.total}
+    def display(self):
+        print(f"{self.customer.name} | {self.food.name} | Qty:{self.qty} | Total: Rs.{self.total}")
