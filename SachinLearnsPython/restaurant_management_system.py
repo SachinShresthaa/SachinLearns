acac7cc780ca0a3e18a9d85da6f4c03ecefdfac2
Sachin Shrestha <sachins.newa1@gmail.com>
2026-08-03 19:51:21 +0545

    food item

--- SachinLearnsPython/restaurant_management_system.py
+++ SachinLearnsPython/restaurant_management_system.py
@@ -3,4 +3,16 @@ import json
 class Person:
     def __init__(self,name):
         self.name=name
-        
+
+class Customer(Person):
+    pass
+
+class FoodItem:
+    def __init__(self,item_id,name,price):
+        self.item_id=item_id
+        self.name=name
+        self.__price=price
+    def get_price(self):
+        return self.__price
+    def display(self):
+        print(f"{self.item_id}. {self.name:<15} Rs.{self.__price}")
